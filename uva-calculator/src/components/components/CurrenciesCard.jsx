c3e2a19fd91b47be48f697a26755237a458082d0
wet333 <wet.4gustin@gmail.com>
2024-11-20 01:35:38 -0300

    Card's padding modification in order to use less screen space

--- uva-calculator/src/components/components/CurrenciesCard.jsx
+++ uva-calculator/src/components/components/CurrenciesCard.jsx
@@ -6,27 +6,30 @@ export default function CurrenciesCard({
     arsPrice = 10000,
     uvaPrice = 10000
 }){
+
+    const itemPadding = "py-2 px-4";
+
     return (
         <Card className="w-full min-w-sm mx-auto">
             <CardHeader>
                 <CardTitle className="text-2xl font-semibold text-center my-2">{title}</CardTitle>
             </CardHeader>
-            <CardContent className="grid gap-4">
-                <div className="flex items-center justify-between p-4 bg-slate-900/50 rounded-lg">
+            <CardContent className="grid gap-3">
+                <div className={`flex items-center justify-between ${itemPadding} bg-slate-900/50 rounded-lg`}>
                     <span className="text-sm font-medium">US Dollars</span>
                     <div className="text-right">
                         <span className="text-lg font-bold">U$D {usdPrice}</span>
                         <span className="sr-only">US Dollars</span>
                     </div>
                 </div>
-                <div className="flex items-center justify-between p-4 bg-slate-900/50 rounded-lg">
+                <div className={`flex items-center justify-between ${itemPadding} bg-slate-900/50 rounded-lg`}>
                     <span className="text-sm font-medium">Argentine Pesos</span>
                     <div className="text-right">
                         <span className="text-lg font-bold">AR$ {arsPrice}</span>
                         <span className="sr-only">Argentine Pesos</span>
                     </div>
                 </div>
-                <div className="flex items-center justify-between p-4 bg-slate-900/50 rounded-lg">
+                <div className={`flex items-center justify-between ${itemPadding} bg-slate-900/50 rounded-lg`}>
                     <span className="text-sm font-medium">UVAs</span>
                     <div className="text-right">
                         <span className="text-lg font-bold">{uvaPrice} UVA</span>
